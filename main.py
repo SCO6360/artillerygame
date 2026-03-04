a49c57c54d9ad645ab7ea41d22ca1d721a791652
SCO6360 <94558690+SCO6360@users.noreply.github.com>
2026-03-03 21:23:58 -0500

    Initialize Pygame and set up display surface

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import sys
+from settings import *
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the display surface (window)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("My First Python Game")
